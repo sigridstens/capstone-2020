9bb89209586caeec9950581c70c5dcfed0f6dc27
Sigrid Stensvold <sigridstens@gmail.com>
2020-09-22 09:19:35 -0500

    Styled hero section on home page. Added animated hero effect.

--- src/home/home.js
+++ src/home/home.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import '../App.css';
+import React, {useEffect, useState} from 'react';
+import './home.css';
 import ExhibitPreview from "../shared/exhibit-preview/exhibitPreview";
 import CallToParticipate from "../shared/call-to-participate/callToParticipate";
 
 
 function Home() {
-    return(
-        <main>
-            <section className="hero content-section">
-                <h2>Everyone has lost experiences.</h2>
-            </section>
+  const wordCarousel = ['experiences', 'opportunities', 'dreams'];
+  const [index, setIndex] = useState(0);
 
-            <section className="content-section">
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(index => {
+        return index === wordCarousel.length - 1 ? 0 : index + 1;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-            </section>
+  return (
+    <main>
+      <section className="hero content-section">
+        <h2>Everyone has <span className="outlined">lost</span> <span className="hidden">experiences.</span>
+          <span className="rotating">{wordCarousel[index]}</span>
+        </h2>
+        <i className="fas fa-angle-down"></i></section>
 
-            <ExhibitPreview/>
-            <CallToParticipate/>
-        </main>
-    )
+      <section className="content-section">
+        The Museum of Lost Experiences is a collection of creative submissions representing those experiences.
+
+      </section>
+
+      <ExhibitPreview/>
+      <CallToParticipate/>
+    </main>
+  )
 }
 
 export default Home;
