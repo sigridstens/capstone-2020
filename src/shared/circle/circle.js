313da71269c246e0cb441169f761dbc96bd36171
Sigrid Stensvold <sigridstens@gmail.com>
2020-11-14 13:17:20 -0600

    Updated submission page to include more user submitted information. Started working on fixing circle submission preview aspect ratio issue.

--- src/shared/circle/circle.js
+++ src/shared/circle/circle.js
@@ -4,11 +4,13 @@ import './circle.css';
 
 function Circle(props) {
   return (
-    <a className="circle" href={props.linkurl}>
-      <div className="overlay"></div>
-      <img className="circle-img" src={process.env.PUBLIC_URL + props.backgroundImage} alt={props.alt}/>
-      <p>{props.title}</p>
-    </a>
+    <div className="circle">
+      <a href={props.linkurl}>
+        <div className="overlay"></div>
+        <img className="circle-img" src={process.env.PUBLIC_URL + props.backgroundImage} alt={props.alt}/>
+        <p>{props.title}</p>
+      </a>
+    </div>
   )
 }
 
