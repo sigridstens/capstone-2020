e4cd2ddb997851e6576c1508e5624e1e72480477
Sigrid Stensvold <sigridstens@gmail.com>
2020-11-30 21:07:42 -0600

    Added initial filtering full-collection.

--- src/full-collection/full-collection.js
+++ src/full-collection/full-collection.js
@@ -7,13 +7,29 @@ import axios from "axios";
 
 function FullCollection() {
   const [submissionData, setSubmissionData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterValue, setFilterValue] = useState({});
+
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get('https://p1vu0ulxhc.execute-api.us-east-2.amazonaws.com/beta/data');
       setSubmissionData(response.data.Items);
+      setFilteredData(response.data.Items);
     }
     fetchData();
     }, []);
+
+  useEffect(() => {
+    setFilteredData(submissionData.filter((submission) => {
+      if (filterValue.age) {
+        if (submission.artistAge !== filterValue.age) return false;
+      }
+      if (filterValue.medium) {
+        if (submission.medium !== filterValue.medium) return false;
+      }
+     return true;
+    }));
+  }, [filterValue]);
 
   return (
     <main>
@@ -23,12 +39,12 @@ function FullCollection() {
           <h3>Entire Collection</h3>
           <h4>Lost Experiences</h4>
 
-          <SubmissionFiltering/>
+          <SubmissionFiltering updateFilter={setFilterValue} currentFilter={filterValue}/>
         </div>
 
         <section className="submission-gallery">
           {
-            submissionData.map((submission, index) => {
+            filteredData.map((submission, index) => {
               return (
                 <Circle key={index} backgroundImage={"https://testsubmissions.s3.us-east-2.amazonaws.com/" + submission.imageName} title={submission.title} linkurl = {submission.linkpath}/>
               )
--- src/shared/submission-filtering/submission-filtering.js
+++ src/shared/submission-filtering/submission-filtering.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './sumission-filtering.css';
 
 
-function SubmissionFiltering() {
+function SubmissionFiltering(props) {
   function toggleOpen() {
     const filterNav = document.getElementsByClassName("filter-nav")[0];
     console.log(filterNav);
 
     filterNav.classList.toggle("nav-open");
+  }
+
+  const updateFilter = (filterType, event) => {
+    const newFilter = {...props.currentFilter};
+    newFilter[filterType] = event.target.value;
+    props.updateFilter(newFilter);
   }
 
   return (
@@ -17,38 +23,37 @@ function SubmissionFiltering() {
         <li>
           <i className="fas fa-search search-icon"/>
         </li>
-        <li>medium
-          <ul className="filter-options">
-            <li>drawing</li>
-            <li>painting</li>
-            <li>collage</li>
-            <li>digital art</li>
-            <li>sculpture</li>
-            <li>fabric, yarn, thread, or textile</li>
-            <li>music or sound</li>
-            <li>photography</li>
-            <li>technology</li>
-            <li>food</li>
-            <li>other</li>
-          </ul>
+        <li>
+          <label>medium
+            <select className="filter-options" onChange={(event) => updateFilter('medium', event)}>
+              <option value="drawing">drawing</option>
+              <option value="painting">painting</option>
+              <option value="collage">collage</option>
+              <option value="digital art">digital art</option>
+              <option value="sculpture">sculpture</option>
+              <option value="fabric, yarn, thread, or textile">fabric, yarn, thread, or textile</option>
+              <option value="music or sound">music or sound</option>
+              <option value="photography">photography</option>
+              <option value="music or sound">music or sound</option>
+              <option value="technology">technology</option>
+              <option value="food">food</option>
+              <option value="other">other</option>
+            </select>
+          </label>
         </li>
-        <li>submitter age
-          <ul className="filter-options">
-            <li>under 20</li>
-            <li>21-30</li>
-            <li>31-40</li>
-            <li>41-50</li>
-            <li>51-60</li>
-            <li>61-70</li>
-            <li>71-80</li>
-            <li>over 80</li>
-          </ul>
-        </li>
-        <li>sort by
-          <ul className="filter-options">
-            <li>age (low to high)</li>
-            <li>date submitted (recent first)</li>
-          </ul>
+        <li>
+          <label>submitter age
+            <select className="filter-options" onChange={(event) => updateFilter('age', event)}>
+              <option value="under 20">under 20</option>
+              <option value="21-30">21-30</option>
+              <option value="31-40">31-40</option>
+              <option value="41-50">41-50</option>
+              <option value="51-60">51-60</option>
+              <option value="61-70">61-70</option>
+              <option value="71-80">71-80</option>
+              <option value="over 80">over 80</option>
+            </select>
+          </label>
         </li>
       </ul>
     </nav>
